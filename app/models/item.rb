931c6bbfef659675177c0f7eee5c3aa18a00db2e
iejima0429 <90872203+iejima0429@users.noreply.github.com>
2021-11-25 19:40:45 +0900

    配送についてプルダウン実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,10 +7,14 @@ class Item < ApplicationRecord
   validates :item_status_id, presence: true
   validates :item_delivery_free_id, presence: true
   validates :shipping_area_id , presence: true
+  validates :days_to_ship_id , presence: true
   validates :price , presence: true, format: { with: /\A[0-9]+\z/ }
 
   has_one_attached :image
   belongs_to :user
   belongs_to :item_category
-  belongs_to :Item_status
+  belongs_to :item_status
+  belongs_to :item_delivery_free
+  belongs_to :shipping_area
+  belongs_to :days_to_ship
 end
--- db/migrate/20211124110955_create_items.rb
+++ db/migrate/20211124110955_create_items.rb
@@ -7,6 +7,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.integer :item_status_id, null: false
       t.integer :item_delivery_free_id, null: false
       t.integer :shipping_area_id , null: false
+      t.integer :days_to_ship_id , null: false
       t.integer :price , null: false
       t.references :user, null: false, foreign_key: true
       t.timestamps
