350a1e9626dbdb0d1c57bb404b8af50392567bbf
iejima0429 <90872203+iejima0429@users.noreply.github.com>
2021-11-25 19:00:31 +0900

    商品の詳細プルダウン実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,24 @@
 class ItemsController < ApplicationController
   def index
+    @item = Item.order("created_at DESC")
   end
   
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:item_category_id)
   end
 end
