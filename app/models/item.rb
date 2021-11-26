88f338308201a89e28c23ed20f15a0a78d72d78a
iejima0429 <90872203+iejima0429@users.noreply.github.com>
2021-11-26 18:48:41 +0900

    エラーメッセージ修正
    プルダウンの’---’は保存できず、エラーメッセージが出るようにした。

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,11 +3,11 @@ class Item < ApplicationRecord
 
   validates :item_name, presence: true
   validates :item_info, presence: true
-  validates :item_category_id, presence: true
-  validates :item_status_id, presence: true
-  validates :item_delivery_free_id, presence: true
-  validates :shipping_area_id , presence: true
-  validates :days_to_ship_id , presence: true
+  validates :item_category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :item_status_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :item_delivery_free_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :shipping_area_id , numericality: { other_than: 1 , message: "can't be blank"}
+  validates :days_to_ship_id , numericality: { other_than: 1 , message: "can't be blank"} 
   validates :price , presence: true, format: { with: /\A[0-9]+\z/ }
   validates :image, presence: true
 
