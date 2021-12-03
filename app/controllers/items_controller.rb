a3492700204e2f9fee45d4333e75dfba35d3abfe
iejima0429 <90872203+iejima0429@users.noreply.github.com>
2021-12-04 06:33:33 +0900

    URLを入力しても他者のページに飛ばない

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
-  before_action :move_to_index, only: [:edit, :update, :destroy]
+ 
   
   def index
     @item = Item.order("created_at DESC")
@@ -27,6 +27,9 @@ class ItemsController < ApplicationController
 
   def edit
    @item = Item.find(params[:id])
+   unless current_user.id == @item.user_id
+    redirect_to action: :index
+  end
   end
 
   def update
@@ -50,10 +53,5 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:item_name, :item_info, :item_category_id, :item_status_id, :item_delivery_free_id, :shipping_area_id, :days_to_ship_id, :image, :price).merge(user_id: current_user.id)
   end
 
-  def move_to_index
-    item = Item.find(params[:id])
-    if user_signed_in?
-      redirect_to action: :index
-    end
-  end
+  
 end
