44ca1bd9585560b7981fd8c416954309088f216e
iejima0429 <90872203+iejima0429@users.noreply.github.com>
2021-12-04 20:38:45 +0900

    商品削除機能修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -27,7 +27,6 @@ class ItemsController < ApplicationController
   end
 
   def edit
-   
    unless current_user.id == @item.user_id
     redirect_to action: :index
   end
@@ -43,9 +42,10 @@ class ItemsController < ApplicationController
   end
 
  def destroy
-   @item = Item.find(params[:id])
    @item.destroy
-   redirect_to root_path
+   unless current_user.id == @item.user_id
+    redirect_to action: :index
+  end
  end
 
   private
