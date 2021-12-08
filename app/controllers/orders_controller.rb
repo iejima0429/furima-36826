ce3a16a26231b63225d5b52c844445ae55951228
iejima0429 <90872203+iejima0429@users.noreply.github.com>
2021-12-08 17:40:02 +0900

    購入内容の表示

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,13 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, except: :index
+
  def index
   @items = Item.all
+  @item = Item.find(params[:item_id])
  end
+
+ def create
+ end
+
+ 
 end
