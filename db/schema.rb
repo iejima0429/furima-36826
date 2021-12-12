f416360edb1132fdc49409c180cb2b0cd2b3c0e4
iejima0429 <90872203+iejima0429@users.noreply.github.com>
2021-12-12 11:59:32 +0900

    商品の購入画面に遷移できるパスの指定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_12_023431) do
+ActiveRecord::Schema.define(version: 2021_12_12_024034) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -31,17 +31,6 @@ ActiveRecord::Schema.define(version: 2021_12_12_023431) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
-
-  create_table "buys", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "post_number", null: false
-    t.integer "shipping_area_id", null: false
-    t.string "city_town_village", null: false
-    t.string "address_number", null: false
-    t.string "buliding_name"
-    t.string "tel_number", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "item_categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
