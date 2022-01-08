6de48a8ae57db34761e1d625be0282cfca8a9a8b
iejima0429 <90872203+iejima0429@users.noreply.github.com>
2022-01-08 13:19:22 +0900

    修正②

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,5 @@ Rails.application.routes.draw do
   resources :items do
     resources :orders, only: [:index, :new, :create]
   end
- end
   resources :items, only: [:index, :new, :create, :show, :edit, :destroy]
 end
